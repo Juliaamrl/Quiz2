074ca4cb88acde2330588e912c403eb113601af8
julie <107113672+Juliaamrl@users.noreply.github.com>
2024-09-06 16:32:00 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -19,6 +19,82 @@ export const perguntas = [
         ]
     },
     {
+        enunciado: "Qual é o seu tipo de herói ideal?",
+        alternativas: [
+            {
+                texto: "Um mago ou bruxa com poderes encantadores.",
+                afirmacao: [
+                    "Você pertence ao universo de Senhor dos Anéis, embarcando em jornadas heroicas.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Um Jedi ou piloto habilidoso com um sabre de luz",
+                afirmacao: [
+                    "Você viveria no universo de Game of Thrones, manobrando intrigas políticas."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "O que você valoriza em uma equipe?",
+        alternativas: [
+            {
+                texto: "Lealdade e habilidades mágicas únicas.",
+                afirmacao: [
+                    "Estudando feitiçarias e vivendo aventuras em uma escola encantadora."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Coragem, habilidades de combate e lealdade",
+                afirmacao: [
+                    "Poderes Jedi e aventuras intergalácticas."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o seu item favorito para uma aventura?",
+        alternativas: [
+            {
+                texto: "Uma varinha mágica ou um grimório de feitiços.",
+                afirmacao: [
+                    "Enfrentando criaturas míticas e lutando contra forças das trevas em uma terra lendária."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Um sabre de luz ou uma nave espacial avançada",
+                afirmacao: [
+                    "Sobrevivendo em um mundo medieval complexo e desafiador."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o seu cenário ideal para uma aventura?",
+        alternativas: [
+            {
+                texto: "Uma escola de magia cheia de mistérios e feitiçaria.",
+                afirmacao: [
+                    "Personagens Harry Potter: Harry, Hermione, Luna, Ronny, Draco Malfoy ."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Uma galáxia distante com planetas exóticos e aventuras espaciais.",
+                afirmacao: [
+                    "Personagens de Star Wars: Yoda, Luke, Darth Vaider, Obi wan Kenobi."
+                ],
+                proxima: 8,
+            },
+        ]
+    {
             {
                 
             
